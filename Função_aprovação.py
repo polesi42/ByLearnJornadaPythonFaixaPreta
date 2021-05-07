89a8d35d6d50a6b72c30ba175ac7fba968e8e23e
polesi42 <83786689+polesi42@users.noreply.github.com>
2021-05-07 14:57:21 -0300

    Update Função_aprovação.py

--- Função_aprovação.py
+++ Função_aprovação.py
@@ -2,30 +2,31 @@
 
 nota1 = 7.5
 nota2 = 4.8
+notas = [nota1, nota2]
 
 #Definimos aqui o que é a função verificar uma aprovação
 
 def verificar_aprovacao() :
-    media = calcular_media([nota1, nota2)]
-    
-    if media >=6:
+    media = calcular_media(nota1, nota2)
+
+    if media >= 6:
        print("O aluno foi aprovado!")
     else:
-       print("O aluno foi reprovado!)
-       
+       print("O aluno foi reprovado!")
+
 #definimos aqui a função calcular a média
 
-def calcular_media(notas):
+def calcular_media(nota1, nota2):
     quantidade = len(notas)
-    
-    soma=0
-    for nota in notas
+
+    soma = 0
+    for nota in notas:
         soma = soma + nota
     media = soma / quantidade
-    
+
     return media
-    
+
 # Agora basta chamar a função verificar aprovação
 
 
-verificar_aprovação()
+verificar_aprovacao()
